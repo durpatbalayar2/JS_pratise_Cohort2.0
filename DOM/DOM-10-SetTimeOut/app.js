fad37b56613a85cb38ad4dd80596bf43521b99e0
Durpat <durpatbalayar2@gmail.com>
2025-11-20 20:08:55 +0530

    add social media

--- DOM/DOM-10-SetTimeOut/app.js
+++ DOM/DOM-10-SetTimeOut/app.js
@@ -12,32 +12,16 @@ btn.addEventListener("click", function () {
   btn.style.padding = "10px 10px";
 
   let rand = 50 + Math.floor(Math.random() * 50);
-
+  console.log(rand)
   let inp = setInterval(() => {
     count++;
     inner.style.width = `${count}%`;
     h2.textContent = `${count}%`;
-
-    //method 1
-
-    //     if (count === 100) {
-    //       clearInterval(inp);
-
-    //       btn.textContent = "Downloaded";
-    //       btn.style.opacity = "0.5";
-    //       btn.style.fontSize = "20px";
-    //       btn.style.padding = "10px 20px";
-    //       requestAnimationFrame(() => {
-    //         requestAnimationFrame(() => {
-    //           alert("Downloaded Successfully!");
-    //         });
-    //       });
-    //     }
   }, rand);
 
-  //   method 2
   setTimeout(() => {
     clearInterval(inp);
+    count = 100;
     btn.textContent = "Downloaded";
     btn.style.opacity = 0.5;
     btn.style.fontSize = "20px";
@@ -52,7 +36,5 @@ btn.addEventListener("click", function () {
     let p = document.createElement("p");
     p.innerHTML = `Download completed in ${rand / 10}<span>s</span>`;
     card.append(p);
-
-    
   }, rand*100);
 });
---
+++ DOM/Own_pratise/app.js
@@ -0,0 +1,7 @@
+//Difference b/w htmlContent,innerText and innerHTMl
+
+let main = document.querySelector("main")
+
+let h1= document.querySelector("h1")
+
+h1.textContent = `<p>Hello Hari How are you</p>`
