27caccdbf188062aded49970599b92ff1f061b0d
Durpat <durpatbalayar2@gmail.com>
2025-11-23 17:21:41 +0530

    FORM handling dynamically

--- DOM/DOM-6-Form/app.js
+++
@@ -1,7 +0,0 @@
-let form= document.querySelector("form")
-
-let inputs= document.querySelectorAll("input")
-form.addEventListener("submit", function(dets){
-    dets.preventDefault(); // prevent page reload
-
-})
