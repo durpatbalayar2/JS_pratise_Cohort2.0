f46a3d3208de32272c8dae1b19b350a48d01d725
Durpat <durpatbalayar2@gmail.com>
2025-11-24 10:12:04 +0530

    Theme Changing with Cookies

--- DOM/Own_pratise/app.js
+++ DOM/Own_pratise/app.js
@@ -1,34 +1,50 @@
-let email = document.querySelector("#email");
-let password = document.querySelector("#password");
-let form = document.querySelector("form");
+let h1 = document.querySelector("h1");
 
-form.addEventListener("submit", function (dets) {
-  dets.preventDefault();
-
-  document.querySelector("#emailError").textContent = "";
-  document.querySelector("#passError").textContent = "";
-
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-  const passwordRegex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/;
-
-  let emailAns = emailRegex.test(email.value);
-  let passwordAns = passwordRegex.test(password.value);
-
-  let isValid = true;
-
-  if (!emailAns) {
-    document.querySelector("#emailError").textContent = "Email is incorrect";
-    isValid = false;
+function setDarkOrLight() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    h1.textContent = " Hii , It's me Dark theme";
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+    h1.textContent = " Hii , It's me Light theme";
   }
+}
 
-  if (!passwordAns) {
-    document.querySelector("#passError").textContent = "Password is incorrect";
-    isValid = false;
-  }
+setDarkOrLight();
 
-  if (isValid) {
-    document.querySelector("#resultMessage").textContent = "Everything is OK";
+// if (localStorage.getItem("theme")) {
+//   document.body.classList.add(localStorage.getItem("theme"));
+// } else {
+//   setDarkOrLight();
+// }
+
+let btn = document.querySelector("#themeBtn");
+btn.addEventListener("click", function () {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    h1.textContent = " Hii , It's me Light theme";
+
+    // localStorage.setItem("theme", "light");
+  } else {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+    h1.textContent = " Hii , It's me Dark theme";
+    // localStorage.setItem("theme", "dark");
   }
 });
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", function () {
+    // if (!localStorage.getItem("theme")) {
+    //   setDarkOrLight();
+    // }
+
+    setDarkOrLight();
+  });
+
+
+  
