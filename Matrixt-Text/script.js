430335126435b140828d0df8004c0814049c1acf
Durpat <durpatbalayar2@gmail.com>
2025-11-28 20:33:12 +0530

    Matrix Text Task Completed ✅

--- Matrixt-Text/script.js
+++ Matrixt-Text/script.js
@@ -1,17 +1,34 @@
-const para = document.querySelector("p");
+const p = document.querySelector("p");
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-const text = para.textContent;
+const text = p.textContent;
+let count = 0;
+let intervalId;
+function randomText() {
+  const str = text
+    .split("")
+    .map((char, idx) => {
+      if (idx < count) {
+        return char;
+      }
+      return characters.split("")[Math.floor(Math.random() * 52)];
+    })
+    .join("");
 
-para.addEventListener("mouseenter", () => {
-  setInterval(() => {
-    const str = text
-      .split("")
-      .map((char, idx) => {
-        return characters.split("")[Math.floor(Math.random() * 53)];
-      })
-      .join("");
+  p.textContent = str;
+  count += 0.25;
 
-    para.textContent = str;
-  }, 30);
+   if (count >= text.length) {
+     clearInterval(intervalId); 
+   }
+}
+
+p.addEventListener("mouseenter", function () {
+  count =0;
+  
+   clearInterval(intervalId); 
+   intervalId = setInterval(randomText, 30);
 });
+
+
+
