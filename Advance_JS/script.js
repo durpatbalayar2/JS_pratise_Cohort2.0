a12953f1811f80c03c420fe4bb89f6ea09a645c3
Durpat <durpatbalayar2@gmail.com>
2025-12-04 22:46:24 +0530

    Opps Section 2 added

--- Advance_JS/script.js
+++ Advance_JS/script.js
@@ -1,78 +1,38 @@
-// SECTION 1: Objects and OOPS Thinking (Foundation)
-// 	1.	Create a user object that stores name and email and has a login method which prints “User logged in”.
-// 	2.	Imagine you now have 5 users.
-// First, think how you would manage them without using a class.
-// Then convert the same logic using a class and observe how the code becomes cleaner. Write code for both approaches.
-// 	3.	Create a product object that stores name and price and has a method which returns the final price after discount.
-
-// The goal of this section is to understand why keeping data and behavior together makes code easier to manage.
+// SECTION 2: Classes and Objects
+// 	4.	Create a Car class with the following:
+// brand
+// speed
+// a drive method that prints the car brand and speed
+// 	5.	Create two different car objects from the same class and verify that their data is different.
+// 	6.	Answer this in your own words:
+// If classes did not exist, how would you write this logic and what problems might occur when the project becomes large?
 
 // Solution
-
-//1.
-let obj = {
-  name: "Sushil",
-  email: "test@gmail.com",
-  loginIn: function () {
-    console.log("User logged in");
-  },
-};
-
-obj.loginIn();
-
-//2. If i have a 5 user then i will create different 5 obj to handle 5 user
-
-let user1 = {
-  name: "Sushil",
-  email: "test@gmail.com",
-  loginIn: function () {
-    console.log("User1 logged in");
-  },
-};
-let user2 = {
-  name: "Ram",
-  email: "test@gmail.com",
-  loginIn: function () {
-    console.log("User2 logged in");
-  },
-};
-
-let user3 = {
-  name: "Sushil",
-  email: "test@gmail.com",
-  loginIn: function () {
-    console.log("User3 logged in");
-  },
-};
-
-user1.loginIn();
-user2.loginIn();
-
-//  Using class
-
-class User {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
+//4.
+class Car {
+  constructor(brand, speed) {
+    this.brand = brand;
+    this.speed = speed;
   }
 
-  login() {
-    console.log("user logged in");
+  drive() {
+    return this.brand + "- " + this.speed;
   }
 }
+let c1 = new Car("Toyata", "120");
 
-let u1 = new User("Ram", "test@gmail.com")
-let u2 = new User("Sita", "test@gmail.com");
+//5.
 
-//3.Create a product object that stores name and price and has a method which returns the final price after discount.
+let c2 = new Car("Thar", "180");
 
-let product = {
-  name: "Iphone",
-  price: 100000,
+//6 Ans:-If classes did not exist, we would need to manually create separate objects for every car with its own properties and methods. We would repeat the same code again and again. When the project becomes large, it will be hard to manage and update. If we want to change something in the drive function, we must update it in every object, which causes duplication and increases the chance of errors. Classes help us reuse the same structure and make the code clean and maintainable.
 
-  discountAmt: function () {
-    return this.price - 2500;
+let car1 = {
+  brand: "Toyata",
+  speed: 120,
+  drive: function () {
+    return this.brand + " -" + this.speed;
   },
 };
 
-console.log(product.discountAmt());
+console.log(car1.drive())
