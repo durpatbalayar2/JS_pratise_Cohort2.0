b5e29db6df5795a567eab53ba45273c9352c0106
Durpat <durpatbalayar2@gmail.com>
2025-12-05 23:30:10 +0530

    Inheritance Concept Oops

--- Advance_JS/script.js
+++ Advance_JS/script.js
@@ -1,31 +1,29 @@
 // Opps Pratise
 
-//1
+// Inheritance Pratise
 
-class Pencil {
-  constructor(name, price, color) {
+class Animal {
+  constructor(name, price) {
     this.name = name;
     this.price = price;
-    this.color = color;
   }
 
-  write(text) {
-    let h1 = document.createElement("h1");
-    h1.innerHTML = text;
-    h1.style.color = this.color;
-    document.body.appendChild(h1);
-  }
-
-  erase() {
-    let allH1 = document.querySelectorAll("h1");
-
-    allH1.forEach((element) => {
-      if(element.style.color === this.color){
-      element.remove();
-      }
-    });
+  eat() {
+    console.log(this.name + "eats food");
   }
 }
 
-let p1 = new Pencil("Natraj", 10, "black");
-let p2 = new Pencil("Doms", 15, "red");
+class Dog extends Animal {
+  constructor(name, price, breed) {
+    super(name, price);
+
+    this.breed = breed;
+  }
+  bark() {
+    console.log(
+      this.name + " Sound too much  and it is a  " + this.breed + "breed"
+    );
+  }
+}
+
+d1 = new Dog("Tommy", 1200, "Germen Soffered");
