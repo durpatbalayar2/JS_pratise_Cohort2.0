239fd61d6ae88cede0ede0970279e4bfa65251f1
Durpat <durpatbalayar2@gmail.com>
2025-12-14 21:37:44 +0530

    Weather Api Pratise

--- Advance_JS/script.js
+++ Advance_JS/script.js
@@ -1,90 +1,22 @@
-// Callback Pratise
+//Weather Api
 
-//1
+async function getCity(city) {
+  try {
+    let apiKey = ` `;
+    let data =
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
+`);
 
-function afterDelay(time, cb) {
-  setTimeout(() => {
-    cb();
-  }, time);
+if(!data.ok){
+  throw new Error("Please enter the correct city and Something went wrong! ")
+}
+    let result = await data.json();
+    console.log(` The temperature of ${city} is ${ Math.floor(result.main.temp)} °C`);
+
+    console.log()
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-afterDelay(3000, function () {
-  console.log("Callback executed...");
-});
-
-//2
-
-function getUser(username, cb) {
-  console.log("getting user data...")
-  setTimeout(() => {
-    cb({ id: 100, name: username });
-  }, 2000);
-}
-
-function getUserPosts(id, cb) {
-  console.log("getting post details...")
-  setTimeout(() => {
-    cb(["hello", "kese ho"]);
-  }, 3000);
-}
-
-getUser("Ramesh", function (data) {
-  getUserPosts(data.id, function (allposts) {
-    console.log(data.username, allposts);
-  });
-});
-
-// Own Scenerio
-
-function CollegeJawwo(username, cb) {
-  console.log("Data Fetching....");
-  setTimeout(() => {
-    cb({ id: "1ST21CS063", name: username });
-  }, 2000);
-}
-
-function getStudentDetail(id, cb) {
-
-  console.log("Student data fetching")
-
-  setTimeout(() => {
-    cb({ rollNo: id });
-  }, 3000);
-}
-
-CollegeJawwo("Sushil", function (data) {
-  console.log(data.name);
-
-  getStudentDetail(data.id, function (details) {
-    console.log(details.rollNo);
-  });
-});
-
-//3.
-
-function loginUser(username, cb) {
-  console.log("User details fetching ................");
-
-  setTimeout(() => {
-    cb({ id: 63, name: username });
-  }, 1000);
-}
-function fetchPermissions(id, cb) {
-  console.log("Permission Fetching...........");
-
-  setTimeout(() => {
-    cb(["Access to learn", "Access to Delete", "Access to Modify"]);
-  }, 2000);
-}
-function loadDashboard(permissions, cb) {
-  console.log("Permission Loaded............");
-  cb();
-}
-
-loginUser("Hari", function (userdata) {
-  fetchPermissions(userdata.id, function (permissions) {
-    loadDashboard(permissions, function () {
-      console.log("Permission Loaded Successfully !");
-    });
-  });
-});
+getCity("Bengaluru");
