02f2e304ade1fe1a0f2e098bc834b1be97b02a79
Durpat <durpatbalayar2@gmail.com>
2025-12-14 22:36:33 +0530

    Error Handling

--- Advance_JS/script.js
+++ Advance_JS/script.js
@@ -1,22 +1,50 @@
-//Weather Api
+//cenario 2 — Bulk Email Sending Simulation with Parallel Promises and Error Handling
 
-async function getCity(city) {
-  try {
-    let apiKey = ` `;
-    let data =
-      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
-`);
+// Simulate sending bulk emails to 5 users. Treat each email-sending operation as a `Promise` (simulate delays with `setTimeout`).
 
-if(!data.ok){
-  throw new Error("Please enter the correct city and Something went wrong! ")
+// ### Requirements
+
+// - Send all emails in parallel using `Promise.all`.
+// - If any email fails (e.g., due to a simulated random failure), catch the error and clearly indicate which specific email failed.
+// - Use a `finally` block to display a message indicating that the "Email process is complete." (regardless of success/failure).
+
+// ### Suggested tasks
+
+// - Create an array of 5 mock email tasks that resolve or reject based on a random condition.
+// - Call `Promise.all` and handle success and failure cases. Show a breakdown of which emails succeeded and which failed.
+// - Ensure the `finally` block runs to update the UI or console indicating completion.
+
+let users = ["ram@gmail.com", "sita@gmail.com", "lovkush@gmail.com"];
+
+function sendEmail(email) {
+  return new Promise((res, rej) => {
+    let time = Math.floor(Math.random() * 5);
+
+    setTimeout(() => {
+      let prob = Math.floor(Math.random() * 10);
+
+      if (prob <= 5) res("Email send successfully");
+      else rej("Email has been rejected");
+    }, time * 1000);
+  });
 }
-    let result = await data.json();
-    console.log(` The temperature of ${city} is ${ Math.floor(result.main.temp)} °C`);
 
-    console.log()
-  } catch (err) {
-    console.log(err.message);
-  }
+async function sendEmails(userlist) {
+  let allresponses = userlist.map(function (email) {
+    return sendEmail(email)
+      .then(function (data) {
+        return data;
+      })
+
+      .catch(function (err) {
+        return err;
+      });
+  });
+
+  let ans = await Promise.all(allresponses);
+  ans.forEach(function (status) {
+    console.log(status);
+  });
 }
 
-getCity("Bengaluru");
+sendEmails(users);
