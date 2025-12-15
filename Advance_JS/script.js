a5ea6f5b01bd48b781423a6c5b72b4d065905f2f
Durpat <durpatbalayar2@gmail.com>
2025-12-15 20:32:09 +0530

    Design Pattern- Factory Pattern Design

--- Advance_JS/script.js
+++ Advance_JS/script.js
@@ -1,27 +1,19 @@
-// Design Pattern 
-//  2. Reavling Module Pattern - Same fnc ko different name se return kar skte hai
+// Design Pattern
+//  2. Factory Module Pattern - Ek normal fnc use karke naye objs create kiye jaate hai, bina new keyword wor constructor use kiye
 
-let userAmt = (function () {
-  let balance = 15000; // private variable
-
-  function balanceAmt() {
-    console.log(balance);
-  }
-
-  function depositeAmt(amount) {
-    balance += amount;
-
-    console.log(balance);
-  }
-
-  function withdrawAmt(amount) {
-    balance -= amount;
-    console.log(balance);
-  }
-
+function createUser(name, age) {
   return {
-    balance: balanceAmt,
-    deposit: depositeAmt,
-    draw: withdrawAmt,
+    name: name,
+    age: age,
+    greet() {
+      return `Hi i am ${name} and I am ${age}years  old. `;
+    },
   };
-})();
+}
+
+// without using new keyword  we can easily create mulitiple objs
+let user1 = createUser("Ram", 25);
+let user2 = createUser("Sita", 22);
+
+console.log(user1.greet());
+console.log(user2.greet());
