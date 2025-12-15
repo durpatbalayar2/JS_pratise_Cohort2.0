de9caced8c9b2b59dbca46e969760f52240a38d2
Durpat <durpatbalayar2@gmail.com>
2025-12-15 20:22:30 +0530

    Design Pattern- Module Pattern

--- Advance_JS/script.js
+++ Advance_JS/script.js
@@ -1,50 +1,28 @@
-//cenario 2 — Bulk Email Sending Simulation with Parallel Promises and Error Handling
+// Design Pattern
 
-// Simulate sending bulk emails to 5 users. Treat each email-sending operation as a `Promise` (simulate delays with `setTimeout`).
+//Module Pattern
 
-// ### Requirements
+let userAmt = (function () {
+  let balance = 15000; // private variable
 
-// - Send all emails in parallel using `Promise.all`.
-// - If any email fails (e.g., due to a simulated random failure), catch the error and clearly indicate which specific email failed.
-// - Use a `finally` block to display a message indicating that the "Email process is complete." (regardless of success/failure).
+  function balanceAmt() {
+    console.log(balance);
+  }
 
-// ### Suggested tasks
+  function depositeAmt(amount) {
+    balance += amount;
 
-// - Create an array of 5 mock email tasks that resolve or reject based on a random condition.
-// - Call `Promise.all` and handle success and failure cases. Show a breakdown of which emails succeeded and which failed.
-// - Ensure the `finally` block runs to update the UI or console indicating completion.
+    console.log(balance);
+  }
 
-let users = ["ram@gmail.com", "sita@gmail.com", "lovkush@gmail.com"];
+  function withdrawAmt(amount) {
+    balance -= amount;
+    console.log(balance);
+  }
 
-function sendEmail(email) {
-  return new Promise((res, rej) => {
-    let time = Math.floor(Math.random() * 5);
-
-    setTimeout(() => {
-      let prob = Math.floor(Math.random() * 10);
-
-      if (prob <= 5) res("Email send successfully");
-      else rej("Email has been rejected");
-    }, time * 1000);
-  });
-}
-
-async function sendEmails(userlist) {
-  let allresponses = userlist.map(function (email) {
-    return sendEmail(email)
-      .then(function (data) {
-        return data;
-      })
-
-      .catch(function (err) {
-        return err;
-      });
-  });
-
-  let ans = await Promise.all(allresponses);
-  ans.forEach(function (status) {
-    console.log(status);
-  });
-}
-
-sendEmails(users);
+  return {
+    balanceAmt,
+    depositeAmt,
+    withdrawAmt,
+  };
+})();
