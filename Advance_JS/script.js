afe67541662e51be8a3f73e3c2a812073b3f7a4c
Durpat <durpatbalayar2@gmail.com>
2025-12-16 22:23:09 +0530

    Lazy Loading

--- Advance_JS/script.js
+++ Advance_JS/script.js
@@ -1,38 +1,27 @@
-// Design Pattern
-//  4. Observer Pattern -> Aisa design pattern jisme ek object ke change hote hi usse conncected saare dusri objs ko automatically notification milna
+const images = document.querySelectorAll(".lazy");
 
-class Channel {
-  constructor() {
-    this.suscribers = []; // Blank array suscribers added
-  }
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
 
-  suscribe(user) {
-    this.suscribers.push(user);
-    user.update(`${user.name} has suscribed the channel`);
-  }
-  unsuscribe(user) {
-    this.suscribers = this.suscribers.filter((sub) => sub !== user);
-    user.update(`${user.name} has unsuscribed your channel`);
-  }
-  notify(message) {
-    this.suscribers.forEach((sub) => sub.update(message));
-  }
-}
+        console.log("Loading:", img.dataset.src);
 
-class User {
-  constructor(name) {
-    this.name = name;
-  }
-  update(data) {
-    console.log(` ${data}`);
-  }
-}
-// channel obj created
-let DBTech = new Channel();
-// user objs created
-let u1 = new User("Ram");
-let u2 = new User("Sita");
+        img.src = img.dataset.src;
 
-DBTech.suscribe(u1);
-DBTech.unsuscribe(u2);
+        img.onload = () => {
+          img.classList.add("loaded");
+        };
 
+        observer.unobserve(img);
+      }
+    });
+  },
+  {
+    root: null,
+    threshold: 0.3,
+  }
+);
+
+images.forEach((img) => observer.observe(img));
